64f921f9bcd6e259753383d03b2f0c0fe29c84d5
anvitha-jain <anvithjain@gmail.com>
2024-06-26 11:06:42 -0700

    [ignore_changes] Made changes to variable names in federation_member module.

--- plugins/module_utils/nd.py
+++ plugins/module_utils/nd.py
@@ -301,7 +301,7 @@ class NDModule(object):
                     self.fail_json(msg="ND Error {code}: {message}".format(**payload), data=data, info=info, payload=payload)
                 elif "messages" in payload and len(payload.get("messages")) > 0:
                     self.fail_json(msg="ND Error {code} ({severity}): {message}".format(**payload["messages"][0]), data=data, info=info, payload=payload)
-                elif "errors" in payload:
+                elif "errors" in payload and len(payload.get("errors", [])) > 0:
                     if ignore_not_found_error:
                         return {}
                     self.fail_json(msg="ND Error: {0}".format(payload["errors"][0]), data=data, info=info, payload=payload)
