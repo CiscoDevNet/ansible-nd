6739aa263c49d8ed3f9d71db812b31f6b1b4a11c
Gaspard Micol <gmicol@cisco.com>
2025-01-10 13:46:08 -0500

    [ignore] Update every aspect of the nd_rest module. Add PATCH method and file as input for the module. Add new tests.

--- plugins/modules/nd_rest.py
+++ plugins/modules/nd_rest.py
@@ -22,11 +22,11 @@ options:
   method:
     description:
     - The HTTP method of the request.
-    - Using C(delete) is typically used for deleting objects.
-    - Using C(get) is typically used for querying objects.
-    - Using C(post) is typically used for modifying objects.
-    - Using C(put) is typically used for modifying existing objects.
-    - Using C(patch) is typically also used for modifying existing objects.
+    - Using C(delete) is used for deleting objects.
+    - Using C(get) is used for querying objects.
+    - Using C(post) is used for modifying objects.
+    - Using C(put) is used for modifying existing objects.
+    - Using C(patch) is also used for modifying existing objects.
     type: str
     choices: [ delete, get, post, put, patch ]
     default: get
@@ -40,15 +40,19 @@ options:
   content:
     description:
     - Sets the payload of the API request directly.
-    - This may be convenient to template simple requests.
-    - For anything complex use the C(template) lookup plugin (see examples).
     type: raw
     aliases: [ payload ]
+  file_path:
+    description:
+    - Name of the absolute path of the filename that includes the body
+      of the HTTP request.
+    - If templated payload is required, use the C(content) parameter
+      together with the C(template) lookup plugin, or use C(template).
+    type: path
+    aliases: [ config_file ]
 extends_documentation_fragment:
 - cisco.nd.modules
-
-notes:
-- Some payloads are known not to be idempotent, so be careful when constructing payloads.
+- cisco.nd.check_mode
 """
 
 EXAMPLES = r"""
@@ -88,7 +92,7 @@ EXAMPLES = r"""
     password: SomeSecretPassword
     path: /nexus/infra/api/aaa/v4/securitydomains/ansible_security_domain_test
     method: get
-  register: quey_one
+  register: query_one
 
 - name: Query all Security Domains using GET method
   cisco.nd.nd_rest:
@@ -97,7 +101,7 @@ EXAMPLES = r"""
     password: SomeSecretPassword
     path: /nexus/infra/api/aaa/v4/securitydomains
     method: get
-  register: quey_all
+  register: query_all
 
 - name: Remove Security Domain using DELETE method
   cisco.nd.nd_rest:
@@ -106,10 +110,50 @@ EXAMPLES = r"""
     password: SomeSecretPassword
     path: /nexus/infra/api/aaa/v4/securitydomains/ansible_security_domain_test
     method: delete
+
+- name: Create a Fabric Policy Template on NDO using POST method
+  cisco.nd.nd_rest:
+    host: nd
+    username: admin
+    password: SomeSecretPassword
+    path: /mso/api/v1/templates
+    method: post
+    content:
+      {
+        "displayName": "ansible_nd_rest_fabric_policies_template",
+        "fabricPolicyTemplate": {},
+        "templateType": "fabricPolicy"
+      }
+  register: create_fabric_policies_template
+
+- name: Update Fabric Policy Template on NDO using PATCH method
+  cisco.nd.nd_rest:
+    host: nd
+    username: admin
+    password: SomeSecretPassword
+    path: "/mso/api/v1/templates/{{ create_fabric_policies_template.current.templateId }}"
+    method: patch
+    content:
+      [
+        {
+          "op": "replace",
+          "path": "/fabricPolicyTemplate/template/domains",
+          "value": [
+            {
+              "name": "ansible_nd_rest_physical_domain",
+              "description": "Ansible nd_rest Physical Domain test for PATCH",
+              "pool": ""
+            }
+          ]
+        }
+      ]
 """
 
 RETURN = r"""
 """
+
+import json
+import os
 
 # Optional, only used for YAML validation
 try:
@@ -120,7 +164,7 @@ except Exception:
     HAS_YAML = False
 
 from ansible.module_utils.basic import AnsibleModule
-from ansible_collections.cisco.nd.plugins.module_utils.nd import NDModule, nd_argument_spec
+from ansible_collections.cisco.nd.plugins.module_utils.nd import NDModule, nd_argument_spec, sanitize
 from ansible.module_utils._text import to_text
 
 
@@ -130,6 +174,7 @@ def main():
         path=dict(type="str", required=True, aliases=["uri"]),
         method=dict(type="str", default="get", choices=["delete", "get", "post", "put", "patch"], aliases=["action"]),
         content=dict(type="raw", aliases=["payload"]),
+        file_path=dict(type="path", aliases=["config_file"]),
     )
 
     module = AnsibleModule(
@@ -139,23 +184,39 @@ def main():
 
     content = module.params.get("content")
     path = module.params.get("path")
+    file_path = module.params.get("config_file")
 
     nd = NDModule(module)
 
-    # Validate content/payload
+    # Report missing file
+    if file_path and not os.path.isfile(file_path):
+        module.fail_json(msg="Cannot find/access file '{0}'".format(file_path))
+
+    # Validate payload/content with PyYAML package
     if content and isinstance(content, str) and HAS_YAML:
         try:
             # Validate YAML/JSON string
             content = yaml.safe_load(content)
         except Exception as e:
-            module.fail_json(msg="Failed to parse provided JSON/YAML payload: %s" % to_text(e), exception=to_text(e), payload=content)
+            module.fail_json(msg="Failed to parse provided YAML/JSON payload: %s" % to_text(e), exception=to_text(e), payload=content)
+    # Validate payload/content only with json Python package
+    elif content and isinstance(content, str):
+        try:
+            # Validate JSON object/string
+            content = json.loads(content)
+        except Exception as e:
+            module.fail_json(
+                msg="Missing PyYAML package to parse YML payload or failed to parse provided JSON payload: %s" % to_text(e),
+                exception=to_text(e),
+                payload=content,
+            )
 
     method = nd.params.get("method").upper()
+    keys_to_sanitize = ["metadata"]
 
     # Append previous state of the object
-    if method in ("PUT", "DELETE"):
-        nd.existing = nd.query_obj(path, ignore_not_found_error=True)
-        nd.previous = nd.existing
+    if method in ("PUT", "DELETE", "PATCH"):
+        nd.existing = nd.previous = sanitize(nd.query_obj(path, ignore_not_found_error=True), keys_to_sanitize)
     if method != "GET":
         nd.result["previous"] = nd.previous
 
@@ -163,12 +224,12 @@ def main():
     if module.check_mode:
         nd.result["jsondata"] = content
     else:
-        nd.result["jsondata"] = nd.request(path, method=method, data=content)
-        nd.existing = nd.result["jsondata"]
+        nd.result["jsondata"] = nd.request(path, method=method, data=content, file=file_path)
+        nd.existing = sanitize(nd.result["jsondata"], keys_to_sanitize)
 
     # Report changes for idempotency depending on methods
     nd.result["status"] = nd.status
-    if nd.result["jsondata"] != nd.previous and method != "GET":
+    if sanitize(nd.result["jsondata"], keys_to_sanitize) != nd.previous and method != "GET":
         nd.result["changed"] = True
 
     # Report success
