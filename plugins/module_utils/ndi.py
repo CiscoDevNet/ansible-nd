cfa447fa261adf76226d534dd099f54fb6951c6e
adealdag <65916351+adealdag@users.noreply.github.com>
2025-01-24 23:26:37 +0100

    [minor_change] Add support for XML file upload pre-change in nd_pcv (DCNE-150) (#77)

--- plugins/module_utils/ndi.py
+++ plugins/module_utils/ndi.py
@@ -307,6 +307,17 @@ class NDI:
             return False
         return True
 
+    def is_xml(self, myxml):
+        try:
+            from lxml import etree
+
+            etree.parse(myxml)
+        except ImportError:
+            self.nd.fail_json(msg="Cannot use lxml etree because lxml module is not available")
+        except etree.XMLSyntaxError:
+            return False
+        return True
+
     def load(self, fh, chunk_size=1024):
         depth = 0
         in_str = False
