49b307b354d143945d889fcac8be749bf6b1714f
Gaspard Micol <gmicol@cisco.com>
2026-01-22 01:04:05 -0500

    [ignore] Pydantic Models: Modify and Clean both local_user.py and base.py based on comments. Add a get method and get_identifier_value function to NDBaseModel.

--- plugins/module_utils/models/base.py
+++ plugins/module_utils/models/base.py
@@ -11,6 +11,7 @@ __metaclass__ = type
 from abc import ABC, abstractmethod
 from pydantic import BaseModel, ConfigDict
 from typing import List, Dict, Any, Optional, ClassVar
+from typing_extensions import Self
 
 
 class NDBaseModel(BaseModel, ABC):
@@ -22,7 +23,7 @@ class NDBaseModel(BaseModel, ABC):
         populate_by_name=True,
     )
 
-    # TODO: find ways to redifine these var in every 
+    # TODO: find ways to redifine these var in every future NDBaseModels
     identifiers: ClassVar[List[str]] = []
     use_composite_identifiers: ClassVar[bool] = False
 
@@ -32,26 +33,28 @@ class NDBaseModel(BaseModel, ABC):
     
     @classmethod
     @abstractmethod
-    def from_response(cls, response: Dict[str, Any]) -> 'NDBaseModel':
+    def from_response(cls, response: Dict[str, Any]) -> Self:
         pass
     
-    # TODO: Modify to make it more generic and Pydantic
+    def get(self, field: str, default: Any = None) -> Any:
+        """Custom get method to mimic dictionary behavior."""
+        return getattr(self, field, default)
+
+    # TODO: Modify to make it more generic and Pydantic | might change and be moved in different Generic Class/Model
     # TODO: add a method to get nested keys, ex: get("spec", {}).get("onboardUrl")
     def get_identifier_value(self) -> Any:
-      """Generates the internal map key based on the selected mode."""
-      #  if self.use_composite_keys:
-      #       # Mode: Composite (Tuple of ALL keys)
-      #       values = []
-      #       for key in self.identifier_keys:
-      #           val = config.get(key)
-      #           if val is None:
-      #               return None # Missing a required part
-      #           values.append(val)
-      #       return tuple(values)
-      #   else:
-      #       # Mode: Priority (First available key)
-      #       for key in self.identifier_keys:
-      #           if key in config:
-      #               return config[key]
-      #       return None
-      pass
+        """Generates the internal map key based on the selected mode."""
+        if self.use_composite_identifiers:
+            # Mode: Composite (Tuple of ALL keys)
+            values = []
+            for identifier in self.identifiers:
+                value = self.get(identifier)
+                if value is None:
+                    return None # Missing a required part | Add Error Handling method here
+                values.append(value)
+            return tuple(values)
+        else:
+            # Mode: Priority (First available key)
+            for identifier in self.identifiers:
+                return self.get(identifier)
+            return None
