ac380feeb9f72a56eb20d2f2c5489c9e7fde071a
Gaspard Micol <gmicol@cisco.com>
2026-03-03 17:19:47 -0500

    [ignore] Rename NDBaseSmartEndpoint to NDBaseEndpoint. Fix importation issues.

--- plugins/module_utils/orchestrators/local_user.py
+++ plugins/module_utils/orchestrators/local_user.py
@@ -8,7 +8,6 @@ from typing import Type
 from ansible_collections.cisco.nd.plugins.module_utils.orchestrators.base import NDBaseOrchestrator
 from ansible_collections.cisco.nd.plugins.module_utils.models.base import NDBaseModel
 from ansible_collections.cisco.nd.plugins.module_utils.models.local_user import LocalUserModel
-<<<<<<< HEAD
 from ansible_collections.cisco.nd.plugins.module_utils.endpoints.base import NDEndpointBaseModel
 from ansible_collections.cisco.nd.plugins.module_utils.orchestrators.types import ResponseType
 from ansible_collections.cisco.nd.plugins.module_utils.endpoints.v1.infra.aaa_local_users import (
@@ -16,34 +15,17 @@ from ansible_collections.cisco.nd.plugins.module_utils.endpoints.v1.infra.aaa_lo
     EpInfraAaaLocalUsersPut,
     EpInfraAaaLocalUsersDelete,
     EpInfraAaaLocalUsersGet,
-=======
-from ansible_collections.cisco.nd.plugins.module_utils.endpoints.base import NDBaseSmartEndpoint
-from ansible_collections.cisco.nd.plugins.module_utils.endpoints.types import ResponseType
-from ansible_collections.cisco.nd.plugins.module_utils.endpoints.v1.infra_aaa_local_users import (
-    V1InfraAaaLocalUsersPost,
-    V1InfraAaaLocalUsersPut,
-    V1InfraAaaLocalUsersDelete,
-    V1InfraAaaLocalUsersGet,
->>>>>>> 8d72e06 ([ignore] Restructure api_endpoints folder into endpoints -> v1. Fix some sanity issues.)
 )
 
 
 class LocalUserOrchestrator(NDBaseOrchestrator):
     model_class: Type[NDBaseModel] = LocalUserModel
 
-<<<<<<< HEAD
     create_endpoint: Type[NDEndpointBaseModel] = EpInfraAaaLocalUsersPost
     update_endpoint: Type[NDEndpointBaseModel] = EpInfraAaaLocalUsersPut
     delete_endpoint: Type[NDEndpointBaseModel] = EpInfraAaaLocalUsersDelete
     query_one_endpoint: Type[NDEndpointBaseModel] = EpInfraAaaLocalUsersGet
     query_all_endpoint: Type[NDEndpointBaseModel] = EpInfraAaaLocalUsersGet
-=======
-    create_endpoint: Type[NDBaseSmartEndpoint] = V1InfraAaaLocalUsersPost
-    update_endpoint: Type[NDBaseSmartEndpoint] = V1InfraAaaLocalUsersPut
-    delete_endpoint: Type[NDBaseSmartEndpoint] = V1InfraAaaLocalUsersDelete
-    query_one_endpoint: Type[NDBaseSmartEndpoint] = V1InfraAaaLocalUsersGet
-    query_all_endpoint: Type[NDBaseSmartEndpoint] = V1InfraAaaLocalUsersGet
->>>>>>> 8d72e06 ([ignore] Restructure api_endpoints folder into endpoints -> v1. Fix some sanity issues.)
 
     def query_all(self) -> ResponseType:
         """
