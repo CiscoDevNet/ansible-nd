354b4eb831c3730270c3d19ac3a770ebca52379c
Allen Robel <arobel@cisco.com>
2026-04-03 12:54:02 -1000

    Fix copyright formatting, remove coding: utf-8
    1. Ansible sanity requires author to be Name (@github_handle)
    
    Changed all files in this PR to:
    
    2. Removed the following legacy Python 2.x remnant:

--- plugins/module_utils/fabric_context.py
+++ plugins/module_utils/fabric_context.py
@@ -1,6 +1,4 @@
-# -*- coding: utf-8 -*-
-
-# Copyright: (c) 2026, Cisco Systems, Inc.
+# Copyright: (c) 2026, Allen Robel (@allenrobel)
 
 # GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
 
