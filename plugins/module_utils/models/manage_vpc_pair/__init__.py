ca904314fe141421e5ecf1a20862a75867fcd24a
Sivakami Sivaraman <sivakasi@cisco.com>
2026-04-07 19:02:06 +0530

    Instead of re-exporting with __init__, it is made empty as per guidelines

--- plugins/module_utils/models/manage_vpc_pair/__init__.py
+++ plugins/module_utils/models/manage_vpc_pair/__init__.py
@@ -1,17 +1,1 @@
-# -*- coding: utf-8 -*-
-#
-# Copyright: (c) 2026, Sivakami S <sivakasi@cisco.com>
-# GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-
-from ansible_collections.cisco.nd.plugins.module_utils.models.manage_vpc_pair.vpc_pair_model import (
-    VpcPairPlaybookConfigModel,
-    VpcPairPlaybookItemModel,
-    VpcPairModel,
-)
-
-__all__ = [
-    "VpcPairModel",
-    "VpcPairPlaybookItemModel",
-    "VpcPairPlaybookConfigModel",
-]
