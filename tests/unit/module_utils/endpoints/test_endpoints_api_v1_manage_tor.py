e7c37b9170bad2c38c9aca6ad6dbd53abb400611
Matt Tarkington <mtarking@cisco.com>
2026-04-09 13:11:58 -0400

    resolve lint errors

--- tests/unit/module_utils/endpoints/test_endpoints_api_v1_manage_tor.py
+++ tests/unit/module_utils/endpoints/test_endpoints_api_v1_manage_tor.py
@@ -23,7 +23,6 @@ from ansible_collections.cisco.nd.tests.unit.module_utils.common_utils import (
     does_not_raise,
 )
 
-
 # =============================================================================
 # Test: EpManageTorAssociatePost
 # =============================================================================
