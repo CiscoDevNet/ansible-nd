7603e3b580c2fae3b2c1c16d586f36bcf618c9e3
Matt Tarkington <mtarking@cisco.com>
2026-05-20 22:12:00 -0400

    fix imports

--- plugins/module_utils/orchestrators/manage_fabric_ibgp_vxlan.py
+++ plugins/module_utils/orchestrators/manage_fabric_ibgp_vxlan.py
@@ -19,7 +19,7 @@ from ansible_collections.cisco.nd.plugins.module_utils.endpoints.v1.manage.manag
     EpManageFabricsPut,
 )
 from ansible_collections.cisco.nd.plugins.module_utils.models.base import NDBaseModel
-from ansible_collections.cisco.nd.plugins.module_utils.models.manage_fabric.manage_fabric_ibgp import FabricIbgpModel
+from ansible_collections.cisco.nd.plugins.module_utils.models.manage_fabric.manage_fabric_ibgp_vxlan import FabricIbgpModel
 from ansible_collections.cisco.nd.plugins.module_utils.orchestrators.base import NDBaseOrchestrator
 from ansible_collections.cisco.nd.plugins.module_utils.orchestrators.types import ResponseType
 
