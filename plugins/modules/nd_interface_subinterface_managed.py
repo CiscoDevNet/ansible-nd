481c6ff4a2fbdca48bf3b4e813ffeed13431bbc6
Allen Robel <arobel@cisco.com>
2026-05-28 08:10:02 -1000

    Bump version_added to 2.0.0 for next ND collection release

--- plugins/modules/nd_interface_subinterface_managed.py
+++ plugins/modules/nd_interface_subinterface_managed.py
@@ -9,7 +9,7 @@ ANSIBLE_METADATA = {"metadata_version": "1.1", "status": ["preview"], "supported
 DOCUMENTATION = r"""
 ---
 module: nd_interface_subinterface_managed
-version_added: "1.6.0"
+version_added: "2.0.0"
 short_description: Manage L3 (managed) subinterfaces on Cisco Nexus Dashboard
 description:
 - Manage L3 subinterfaces (managed variant) on Cisco Nexus Dashboard.
