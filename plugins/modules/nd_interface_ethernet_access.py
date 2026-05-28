9358959ec56ee8e3bde62301b75acd40f6e92936
Allen Robel <arobel@cisco.com>
2026-05-28 08:09:27 -1000

    Bump version_added to 2.0.0 for next ND collection release

--- plugins/modules/nd_interface_ethernet_access.py
+++ plugins/modules/nd_interface_ethernet_access.py
@@ -9,7 +9,7 @@ ANSIBLE_METADATA = {"metadata_version": "1.1", "status": ["preview"], "supported
 DOCUMENTATION = r"""
 ---
 module: nd_interface_ethernet_access
-version_added: "1.4.0"
+version_added: "2.0.0"
 short_description: Manage ethernet accessHost interfaces on Cisco Nexus Dashboard
 description:
 - Manage ethernet accessHost interfaces on Cisco Nexus Dashboard.
