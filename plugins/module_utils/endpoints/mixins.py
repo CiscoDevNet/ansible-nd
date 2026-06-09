caf03999aa603bc93ec13de066b9a8ef1a932dfa
L Nikhil Sri Krishna <nisaikri@cisco.com>
2026-06-09 14:53:03 +0530

    fix(mixins): remove duplicate OffsetMixin after PR #271 merge

--- plugins/module_utils/endpoints/mixins.py
+++ plugins/module_utils/endpoints/mixins.py
@@ -108,12 +108,6 @@ class PolicyGroupIdMixin(BaseModel):
     )
 
 
-class OffsetMixin(BaseModel):
-    """Mixin for endpoints that require a pagination offset parameter."""
-
-    offset: Optional[int] = Field(default=None, ge=0, description="Pagination offset")
-
-
 class SwitchSerialNumberMixin(BaseModel):
     """Mixin for endpoints that require switch_sn parameter."""
 
