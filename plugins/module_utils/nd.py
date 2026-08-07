4d4d4d41d62510c29e13a7a261dbcc174112a0b4
Matt Tarkington <mtarking@cisco.com>
2026-08-07 12:30:18 -0400

    Remove legacy /version.json dependency from nd httpapi
    The unified Nexus Dashboard (4.2+) no longer exposes /version.json. Drop the get_version(platform="nd") branch in the httpapi plugin and the now-unused NDModule.version property that consumed it. The NDFC version path (get_version("ndfc")) used by cisco.dcnm modules is unchanged.

--- plugins/module_utils/nd.py
+++ plugins/module_utils/nd.py
@@ -154,7 +154,6 @@ class NDModule(object):
         self.url = None
         self.httpapi_logs = list()
         self.connection = None
-        self._version = None
 
         # Set Connection plugin
         self.set_connection()
@@ -162,12 +161,6 @@ class NDModule(object):
         if self.module._debug:
             self.module.warn("Enable debug output because ANSIBLE_DEBUG was set.")
             self.params["output_level"] = "debug"
-
-    @property
-    def version(self):
-        if self._version is None and self.connection:
-            self._version = self.connection.get_version("nd")
-        return self._version
 
     def set_connection(self):
         if self.connection is None:
